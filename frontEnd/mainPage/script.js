80b9cde3825b77c6b3e71af102c8041878cc3c47
ItsCOMMANDer <111245538+ItsCOMMANDer@users.noreply.github.com>
2023-08-21 20:51:35 +0200

    EEEEEEEEEEEEEEEEEEEEEEEEE

---
+++ frontEnd/mainPage/script.js
@@ -0,0 +1,10 @@
+function toggleHamMenu() {
+      const menuButton = document.getElementById("menuItems");
+
+      if(menuButton.style.display == "block") {
+        menuButton.style.display = "none";
+      } else {
+        menuButton.style.display = "block";
+      }
+
+}
