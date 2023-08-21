0f5c7023fb3612887b50c2c355400f19d1fbce54
ItsCOMMANDer <111245538+ItsCOMMANDer@users.noreply.github.com>
2023-08-21 21:46:04 +0200

    e

--- frontEnd/mainPage/script.js
+++ frontEnd/mainPage/script.js
@@ -2,8 +2,10 @@ function toggleHamMenu() {
       const menuButton = document.getElementById("menuItems");
 
       if(menuButton.style.display == "block") {
+        new Audio("./files/disappear.mp3").play();
         menuButton.style.display = "none";
       } else {
+        new Audio("./files/plop.mp3").play();
         menuButton.style.display = "block";
       }
 
